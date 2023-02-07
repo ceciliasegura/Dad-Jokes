0650c45c6dffad8c391ec55af8b05eb6c5d2c164
ceci <ceciliasegura.t@hotmail.com>
2023-02-07 17:11:27 +0100

    First commmit

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,38 @@
+
+import {useState, useEffect} from "react"
+
+
+export default function CardJokes(){ 
+    const [jokes, setjokes] = useState(null) 
+
+    useEffect(() =>{
+        fetch ("https://icanhazdadjoke.com",{headers:{
+            "Accept":"application/json"}
+            
+        })
+        
+        .then(res => res.json())
+        .then (data => setjokes(data))
+        .catch (error => error) 
+    }, []);
+
+    const test = () => {
+        fetch ("https://icanhazdadjoke.com",{headers:{
+           "Accept":"application/json" 
+            }  
+       }) 
+
+        .then(res => res.json())
+        .then (data => setjokes(data))
+
+    }  
+
+        return ( 
+            
+            <div className="card-jokes"> 
+                <p>{jokes? jokes.joke : "."}</p> 
+                <button onClick={test}>Next</button> 
+            </div>
+            
+        )
+}
